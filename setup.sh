987532c17a99cd6dcf379a9618b97be27db0f9bc
Jeff Bolin <ji9@qad.com>
2022-11-29 17:54:37 -0800

    Finished setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,17 @@
 #!/usr/bin/env bash
 
-echo "Hello World"
+echo "Setting up my development environment..."
 
-curl -o ~/test.bashrc https://raw.githubusercontent.com/SpaceCowboy113/dev2/master/.bashrc
-curl -o ~/test.inputrc https://raw.githubusercontent.com/SpaceCowboy113/dev2/master/.inputrc
-curl -o ~/test.tmux.conf https://raw.githubusercontent.com/SpaceCowboy113/dev2/master/.tmux.conf
-curl -o ~/test.vimrc https://raw.githubusercontent.com/SpaceCowboy113/dev2/master/.vimrc
+curl -o ~/.bashrc https://raw.githubusercontent.com/SpaceCowboy113/dev2/master/.bashrc
+curl -o ~/.inputrc https://raw.githubusercontent.com/SpaceCowboy113/dev2/master/.inputrc
+curl -o ~/.tmux.conf https://raw.githubusercontent.com/SpaceCowboy113/dev2/master/.tmux.conf
+curl -o ~/.vimrc https://raw.githubusercontent.com/SpaceCowboy113/dev2/master/.vimrc
 
-sudo yum install tmux
+sudo yum -y install tmux
+
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+vim +PluginInstall +qall
+
+source ~/.bashrc
+bind -f ~/.inputrc
+tmux source-file ~/.tmux.conf
