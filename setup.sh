90bb3691165851b4fea354f9c6153ce39f5db3fe
Jeff Bolin <ji9@qad.com>
2022-11-29 16:54:33 -0800

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -2,4 +2,14 @@
 
 echo "Hello World"
 
-wget -O - https://github.com/SpaceCowboy113/workDev/blob/master/.bashrc
+wget https://github.com/SpaceCowboy113/dev2/blob/master/.bashrc
+wget https://github.com/SpaceCowboy113/dev2/blob/master/.inputrc
+wget https://github.com/SpaceCowboy113/dev2/blob/master/.tmux.conf
+wget https://github.com/SpaceCowboy113/dev2/blob/master/.vimrc
+
+mv .bashrc ~
+mv .inputrc ~
+mv .tmux.conf ~
+mv .vimrc ~
+
+source ~/.bashrc
